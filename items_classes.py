6fba5429a544f71bd8ca5b9170e73203070730bd
Mary Mbugua <marymbugua.nm@gmail.com>
2018-04-26 15:23:34 +0300

    created wall class

--- items_classes.py
+++ items_classes.py
@@ -39,3 +39,48 @@ class Item(pygame.sprite.Sprite):
         '''plays sound and returns itemType'''
         self.play_sound()
         return self.itemType
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, name, x, y, width, height):
+        '''name: must be a unique string in order to be loaded correctly
+        x: x coordinate
+        y: y coordinate
+        width: the width of the surface
+        height: the height of the surface'''
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def get_x(self):
+        return self.rect.x
+
+    def get_y(self):
+        return self.rect.y
+
+    def get_rect(self):
+        return self.rect
+
+    def get_bottom(self):
+        return self.rect.bottom
+
+    def get_top(self):
+        return self.rect.top
+
+    def get_left(self):
+        return self.rect.left
+
+    def get_right(self):
+        return self.rect.right
+
+    def get_width(self):
+        return self.rect.width
+
+    def get_height(self):
+        return self.rect.height
